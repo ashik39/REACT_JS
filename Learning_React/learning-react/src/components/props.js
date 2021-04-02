02ce4731d4e360da4f044988551bb5a2df48eaf3
unknown <asanashik7@gmail.com>
2021-04-02 16:34:12 +0530

    props concept implemented in a component 'props.js'

---
+++ Learning_React/learning-react/src/components/props.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Greet(props){
+    console.log(props)
+    return (
+        <div>
+            <h1>
+                Hello {props.name} a.k.a {props.heroName}
+            </h1>
+        </div>
+    )
+}
+
+export default Greet
