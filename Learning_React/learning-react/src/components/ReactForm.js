c152e8cc1f94a407029c37e130713a095e284dec
unknown <asanashik7@gmail.com>
2021-04-08 18:54:25 +0530

    ReactForm component modified for proper naming convention and semicolons

--- Learning_React/learning-react/src/components/ReactForm.js
+++ Learning_React/learning-react/src/components/ReactForm.js
@@ -1,33 +1,112 @@
 import React,{ useState } from 'react'
 
 function ReactForm(){
-    const [name,setName] = useState('')
-    const [email,setEmail] = useState('')
+    const [name,setName] = useState('');
+    const [email,setEmail] = useState('');
+    const [gender,setGender] = useState('');
+    const [address,setAddress] = useState('');
+    const [nameError,setNameError] = useState('');
+    const [emailError,setEmailError] = useState('');
+    const [genderError,setGenderError] = useState('');
+    const [addressError,setAddressError] = useState('');
 
-    function Change(event){
-        const Name = event.target.name
-        const Email = event.target.name
+    function validate(){
+        
+        if (name.trim() === ''){
+            setNameError('*Name cant be blank');
+        }
+        else{
+            setNameError(''); 
+        }
+        if(email.trim() === ''){
+            setEmailError('*Email cant be blank');
+            
+        }
+        else{
+            setEmailError('');
+        }
 
-        setName(Name)
-        setEmail(Email)
+        if(gender.trim() === ''){
+            setGenderError('*Select Gender');
+            
+        }
+        else{
+            setGenderError('');
+        }
+
+        if(address.trim() === ''){
+            setAddressError('*Address cant be blank');
+            return false;
+        }
+        else{
+            setAddressError('');
+            return true;
+        }
 
     }
 
-    function FormSubmit(event){
-        event.preventDefault()
+    function Change(event){
+        const name = event.target.name;
+        const value = event.target.value;
+        switch (name){
+            case "name":
+                setName(value);
+                break;
+            case "email":
+                setEmail(value);
+                break;
+            case "gender":
+                setGender(value);
+                break;
+            case "address":
+                setAddress(value);
+                break;
+            default:
+                return;
+            }
+    }
+
+    function formSubmit(event){
+        
+        event.preventDefault();
+        const isValidate = validate();
+        if(isValidate){
         alert(`Form Submitted \n
         Your Name : ${name} \n
-        Your Email : ${email}`)
+        Your Email : ${email} \n
+        Your Gender : ${gender}\n
+        Your Address : ${address}`);
+        }        
     }
 
     return (
-        <div>
-            <form onSubmit = {FormSubmit}>
-                <input type ="text" name="name" placeholder="Name" onChange = {Change}/><br/>
-                <p></p>
-                <input type ="email" name="email" placeholder="Email" onChange = {Change} /><br/>
-                <p></p>
-                <input type ="submit" value = "SUBMIT" />
+        <div className="Form">
+            <h1>BASIC FORM</h1>
+            <form onSubmit = {formSubmit}>
+
+                <label for="exampleInputEmail1" className="form-label">Name :</label>
+                <input type ="text" className="form-control width" id="exampleInputEmail1" aria-describedby="emailHelp"
+                name="name" placeholder="Name" value={name} onChange = {Change}/>                
+                <p style = {{color : 'red'}}>{nameError}</p>
+
+                <label for="exampleInputEmail1" className="form-label">Email :</label>
+                <input className="form-control width" type="email" id="exampleInputEmail1" aria-describedby="emailHelp" 
+                name="email" placeholder="Email" value={email} onChange = {Change}/>
+                <p style = {{color : 'red'}}>{emailError}</p>
+                
+                <label>Gender : </label>
+                <select className="form-select width" name="gender" value={gender} onChange = {Change}>
+                    <option value = "">SELECT</option>
+                    <option value = "MALE">MALE</option>
+                    <option value = "FEMALE">FEMALE</option>
+                </select>
+                <p style = {{color : 'red'}}>{genderError}</p><br/>
+                
+                <label className="form-label">Address : </label>
+                <textarea className="form-control width" name ="address" value={address} placeholder="Address" onChange = {Change}/>
+                <p style = {{color : 'red'}}>{addressError}</p><br/>
+                
+                <input className="btn btn-primary" type ="submit" value = "SUBMIT" />
             </form>
         </div>
     )
